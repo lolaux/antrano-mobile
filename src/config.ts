1392022aff8d2b59393f6ef6df43d6b5a8e1f88b
sudip-biswas <sudipid@gmail.com>
2021-09-13 09:02:55 -0700

    change API url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
-const DEV_API_URL = 'https://dev.antrano.com';
-const PROD_API_URL = 'https://prod.antrano.com';
+const DEV_API_URL = 'http://ec2-3-12-154-181.us-east-2.compute.amazonaws.com:5000/api/';
+const PROD_API_URL = 'http://ec2-3-12-154-181.us-east-2.compute.amazonaws.com:5000/api/';
+const IMAGE_BASE_URL = "http://3.21.185.32:3000/images/product/";
+const IMAGE_BASE_BANNER = "http://3.21.185.32:3000/images/banner/"
 
 const dev = {
     apiURL: DEV_API_URL,
